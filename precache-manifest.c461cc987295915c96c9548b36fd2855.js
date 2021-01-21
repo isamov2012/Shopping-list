76c73fcccb60cdf9c8950c5a53603db7288a4df7
Isam <isamov2012@hotmail.com>
2021-01-21 22:27:10 +0100

    Updates

---
+++ precache-manifest.c461cc987295915c96c9548b36fd2855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf92d2b42e81f1cca47191965e9308d0",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "d72fc41754edf2aec3df",
+    "url": "/shopping-list/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "d72fc41754edf2aec3df",
+    "url": "/shopping-list/static/js/2.6c59d009.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-list/static/js/2.6c59d009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e91769c87499bf724be",
+    "url": "/shopping-list/static/js/main.0f200b89.chunk.js"
+  },
+  {
+    "revision": "f6d252fd82d3a1318e00",
+    "url": "/shopping-list/static/js/runtime-main.7499d5db.js"
+  }
+]);
